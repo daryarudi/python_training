45e3cc9b801b8a09010c27e8fdc8bc779b797362
Darya <darya.rudi@yandex.ru>
2019-08-29 17:56:14 +0300

    Сделано общее создание фикстуры, выполнение тестов делается в одной сессии

--- fixture/application.py
+++ fixture/application.py
@@ -9,9 +9,9 @@ class Application:
     def __init__(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
-        self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.session = SessionHelper(self)
 
     def destroy(self):
         wd = self.wd
@@ -19,4 +19,4 @@ class Application:
 
     def open_home_page(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/group.php")
+        wd.get("http://localhost/addressbook/")
--- fixture/contact.py
+++ fixture/contact.py
@@ -7,13 +7,14 @@ class ContactHelper:
         self.app = app
 
     def create(self, contact):
-        wd = self.wd
+        wd = self.app.wd
         self.open_add_contact_page()
         self.enter_contact_info(contact)
         wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
+        self.open_add_contact_page()
 
     def enter_contact_info(self, contact):
-        wd = self.wd
+        wd = self.app.wd
         wd.find_element_by_name("firstname").send_keys(contact.fname)
         wd.find_element_by_name("middlename").send_keys(contact.mname)
         wd.find_element_by_name("lastname").send_keys(contact.lname)
@@ -23,5 +24,5 @@ class ContactHelper:
         Select(wd.find_element_by_name("new_group")).select_by_index(contact.group)
 
     def open_add_contact_page(self):
-        wd = self.wd
+        wd = self.app.wd
         wd.find_element_by_link_text("add new").click()
