102058d482e4e68ad1374e696d6616c253e0caf6
Darya <darya.rudi@yandex.ru>
2019-09-13 14:34:55 +0300

    Добавлены интеллектуальные функции сессии

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -4,6 +4,7 @@ from model.group import Group
 
 def test_add_group(app):
     app.group.create(Group(name="test", header="test test", footer="test teest"))
+    app.session.logout()
 
 
 def test_add_empty_group(app):
